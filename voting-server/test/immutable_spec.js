1e180bdddd39265fc81bd7d6c5130715cb6714ef
Joe Karasek <joseph.karasek@gmail.com>
2016-04-16 12:13:23 -0700

    set up mocha tests

---
+++ voting-server/test/immutable_spec.js
@@ -0,0 +1,51 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+describe('immutability', () => {
+
+  describe('a number', () => {
+
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', () => {
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+
+  });
+
+  describe('A List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+      let nextNextState = addMovie(nextState, '28 Weeks Later');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+      expect(nextNextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine',
+        '28 Weeks Later'
+      ));
+    });
+
+  });
+});
